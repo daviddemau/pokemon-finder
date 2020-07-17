b03579acf701c08ed2da60b743646ade1dae5a65
d.demau <david.demau@gmail.com>
2020-07-17 12:20:37 +0200

    improved project structure and file names

--- pokedex.js
+++ pokedex.js
@@ -1,65 +1,61 @@
-//getting data from Github repository
-var url = 'https://raw.githubusercontent.com/Biuni/PokemonGO-pokedex/master/pokedex.json';
+// getting data from pokemon API
+const baseUrl = 'https://raw.githubusercontent.com/Biuni/PokemonGO-pokedex/master/pokedex.json';
 const pokemons = [];
 
-window.fetch(url)
-.then(blob => blob.json())
-.then(data => pokemons.push(...data.pokemon))
+window.fetch(baseUrl)
+    .then(blob => blob.json())
+    .then(data => pokemons.push(...data.pokemon))
 
-//variables
-var input = document.querySelector('input');
-var list = document.querySelector('ul');
-var playing = false;
-var audio = new Audio('sounds/101-opening.mp3');
+// variables
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
+let playing = false;
+const audio = new Audio('sounds/pokemon-opening.mp3');
 
-//input.addEventListener('change', printResults);
+// display result when typing on search bar
 input.addEventListener('keyup', printResults);
 
-//play music button
-var musicToggle = document.querySelector('button');
+// music management
+const musicToggle = document.querySelector('button');
 musicToggle.addEventListener('click', playMusic);
 
-//functions
+// filtering results by name and type
 function findPokemons(search, pokemons) {
-  return pokemons.filter(pokemon => {
-    var regex = new RegExp(search, 'gi');
-    return pokemon.name.match(regex) || pokemon.type.find(a => a.match(regex));
-  });
+    return pokemons.filter(pokemon => {
+        const regex = new RegExp(search, 'gi');
+        return pokemon.name.match(regex) || pokemon.type.find(a => a.match(regex));
+    });
 }
 
 function printResults() {
-  list.innerHTML = '';
-  var result = findPokemons(this.value, pokemons);
-  result.map(function(element) {
-    list.innerHTML += '<li class="name">' + '<img src="' + element.img + '"/>' + '<span>' + element.name + '</span>' + '<span>' + element.height + '</span>' + '<span>' + element.type + '</span>' + '</li>' + '<li class="details">'
-    + '<span>' + '<p class="detail-title">' + 'spawn chance: ' + '</p>' + element.spawn_chance  + '</span>' + '<span>' + '<p class="detail-title">' + 'weight: ' + '</p>' + element.weight +'</span>' + '<span>'
-    + '<p class="detail-title">' + 'weaknesses: ' + '</p>' + element.weaknesses +'</span>' + '</li>'
-  });
+    list.innerHTML = '';
+    const result = findPokemons(this.value, pokemons);
+    result.map(function (element) {
+        list.innerHTML += '<li class="name">' + '<img src="' + element.img + '"/>' + '<span>' + element.name + '</span>' + '<span>' + element.height + '</span>' + '<span>' + element.type + '</span>' + '</li>' + '<li class="details">'
+            + '<span>' + '<p class="detail-title">' + 'spawn chance: ' + '</p>' + element.spawn_chance + '</span>' + '<span>' + '<p class="detail-title">' + 'weight: ' + '</p>' + element.weight + '</span>' + '<span>'
+            + '<p class="detail-title">' + 'weaknesses: ' + '</p>' + element.weaknesses + '</span>' + '</li>'
+    });
 
-  //toggle more details about specific pokemon
-  let pokemonNames = document.querySelectorAll('.name');
-  Array.from(pokemonNames).forEach(toggleShow);
+    //toggle more details about specific pokemon
+    let pokemonNames = document.querySelectorAll('.name');
+    Array.from(pokemonNames).forEach(togglePokemonDetail);
 }
 
-function toggleShow(element) {
-  element.addEventListener('click', () => {
-    if(element.nextSibling.style.display == '') {
-      element.nextSibling.style.display = 'block';
-    } else {
-      element.nextSibling.style.display = '';
-    }
-  })
+function togglePokemonDetail(element) {
+    element.addEventListener('click', () => {
+        element.nextSibling.style.display === '' ?
+            element.nextSibling.style.display = 'block' :
+            element.nextSibling.style.display = '';
+    })
 }
 
 function playMusic() {
-  if(!playing) {
-    audio.play();
-    musicToggle.innerHTML = 'Stop Music';
-    playing = true;
-  } else {
-    audio.pause();
-    audio.currentTime = 0;
-    musicToggle.innerHTML = 'Play Music';
-    playing = false;
-  }
+    playing = !playing;
+    if (playing) {
+      audio.play();
+      musicToggle.innerHTML = 'Stop Music';
+    } else {
+      audio.pause();
+      musicToggle.innerHTML = 'Play Music';
+    }
 }
